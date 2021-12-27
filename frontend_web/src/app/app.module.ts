35767c53a56bb273cdc568b9bcf2548324d32111
Thiago Lima <thg.limadesousa@gmail.com>
2021-12-26 22:26:40 -0300

    Organiza os imports

--- frontend_web/src/app/app.module.ts
+++ frontend_web/src/app/app.module.ts
@@ -4,14 +4,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/page/login/login.component';
 import { Pg404Component } from './components/page/pg404/pg404.component';
-import { HomeComponent } from './components/home/home.component';
+import { BaseComponent } from './components/template/base/base.component';
 import { ListCategoryComponent } from './components/category/list-category/list-category.component';
 import { ListQuestionComponent } from './components/question/list-question/list-question.component';
 import { CategoryManagerComponent } from './components/category/category-manager/category-manager.component';
 import { QuestionManagerComponent } from './components/question/question-manager/question-manager.component';
-import { BaseComponent } from './components/template/base/base.component';
 
 @NgModule(
   {
