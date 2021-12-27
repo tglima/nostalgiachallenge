594f161acafbee6caea69a15e398056f7b3b403d
Thiago Lima <thg.limadesousa@gmail.com>
2021-12-26 21:54:29 -0300

    Adiciona imports referente ao componente login

--- frontend_web/src/app/app.module.ts
+++ frontend_web/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { LoginComponent } from './components/page/login/login.component';
 
 @NgModule(
   {
     declarations: [
-      AppComponent
+      AppComponent,
+      LoginComponent
     ],
-    imports: [
-      BrowserModule,
-      AppRoutingModule
-    ],
+    imports:
+      [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        HttpClientModule
+      ],
     providers: [],
     bootstrap: [AppComponent]
   })
